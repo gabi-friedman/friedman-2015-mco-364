d9e44055b094d65bea0d16566f4446a009bf3f01
gabi-friedman <gcf613@gmail.com>
2015-02-18 05:19:28 -0500

    blinking, resizing smiley completed

--- src/friedman/smile/Smile.java
+++ src/friedman/smile/Smile.java
@@ -6,21 +6,26 @@ import java.awt.Container;
 import javax.swing.JFrame;
 
 public class Smile extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Smile(){
-		setSize(800,600); //width then height
+		setSize(400,400); //width then height
 		setTitle("SMILE :)");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		Container  contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
-
-		contentPane.add(new SmileComponant(),BorderLayout.CENTER);
+		SmileComponant sc = new SmileComponant();
+		
+		contentPane.add(sc,BorderLayout.CENTER);
 	}
 
 	public static void main(String[] args){
 		Smile smile = new Smile();
 		smile.setVisible(true);
-
 		//animation thread 
 		Thread t = new Thread(){
 			public void run(){
--- src/friedman/smile/SmileComponant.java
+++ src/friedman/smile/SmileComponant.java
@@ -7,53 +7,68 @@ import javax.swing.JComponent;
 
 public class SmileComponant extends JComponent {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//private boolean wink = false;
 	boolean wink = false;
-	int x = 167;
-	
+	private double x;
+	private int w;
+	private int h;
+	private boolean firstRound = true;
+
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		// TODO Auto-generated method stub
 		super.paintComponent(g);
-		
+
+		w = getWidth();
+		h = getHeight();
+		if(firstRound){
+			firstRound = false;
+			x = h * .08;
+		}
+
 		g.setColor(Color.YELLOW);
-		g.fillOval(200, 100, 400, 400);
+		g.fillOval((int) (w * .01), (int) (h * .01), (int) (w * .98), (int) (h * .98));
 		g.setColor(Color.RED);
-		g.drawArc(250, 150, 300, 300, 190, 160);
+		g.drawArc((int) ((w * .3)), (int) ((h * .02)), (int) ((w * .4)), (int) ((h * .7)), 190, 160);
 		g.setColor(Color.GREEN);
-		g.fillOval(430,205, 50, 50);
-		g.fillOval(330,205, 50, 50);
+		g.fillOval((int) (w * .38), (int) (h * .18), (int) (w * .1), (int) (h * .1));
+		g.fillOval((int) (w * .58), (int) (h * .18), (int) (w * .1), (int) (h * .1));
+
 		if(!wink){
 			g.setColor(Color.YELLOW);
-			g.fillOval(430, x++, 50, 50);
-			g.setColor(Color.BLACK);
-			g.drawArc(430, x, 50, 50, 180, 180);
-		if(x == 207){
-			x= 167;
-			g.setColor(Color.RED);
-			g.drawLine(285, 327, 225, 327);
-			g.drawLine(575, 327, 515, 327);		
+			g.fillOval((int) (w*.58), (int) x++, (int) (w*.1), (int) (h*.1));
+			if(x >= h*.18){
+				x = h*.08;
+			}
+			wink = false;	
 		}
-		wink = false;
-	}
-		
-		
-		/*if(!wink){
-			g.fillOval(330,205, 50, 50);
-			wink=true;
+
+		/*if (!wink) {
+			g.setColor(Color.PINK);
+			g.fillOval((int) (w * .58), (int) (h * x), (int) (w * .1), (int) (w * .1));
+			// g.setColor(Color.BLACK);
+			// g.drawArc((int)(w*.58), x, 50, 50, 180, 180);
+
+			if (x >= (int) (h *.18)) {
+				g.setColor(Color.BLUE);
+				x = .01;
+			}
+			x = x + 1;
 		}
-		else{
-			g.setColor(Color.black);
-			g.drawArc(325, 195, 60, 60, 175, 190);
-			wink=false;
-		}*/
 
+		wink = !wink;
+		 */
 
-
+		/*
+		 * if(!wink){ g.fillOval(330,205, 50, 50); wink=true; } else{
+		 * g.setColor(Color.black); g.drawArc(325, 195, 60, 60, 175, 190);
+		 * wink=false; }
+		 */
 
 	}
-
-
-
 
 }
